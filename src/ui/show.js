bb762acbd091d0a71cf0433d75372e651d0abb1e
Apurva Thakur <apurva@frrole.com>
2021-02-15 12:20:52 +0530

    final build

--- src/ui/show.js
+++ src/ui/show.js
@@ -64,7 +64,7 @@ export default class show extends Component{
         axios.patch(requrl, dataUpdate, {headers:headers})
         .then(res => {
             console.log(res); 
-            window.location.reload();
+            this.props.history.push("/memes");
         })
         .catch((err) => {
             console.log("ERROR: ====", err);
